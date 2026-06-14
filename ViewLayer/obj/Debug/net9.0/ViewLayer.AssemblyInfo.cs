f14ef8e84e68f8aa2ca41b7528a8660124338dad
Elias3052006 <e.coronelrizzo@student.fontys.nl>
2026-06-14 16:22:43 +0200

    update

--- ViewLayer/obj/Debug/net9.0/ViewLayer.AssemblyInfo.cs
+++ ViewLayer/obj/Debug/net9.0/ViewLayer.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ViewLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+379634cfd2dceceb083b8505d3f88e99c93192ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("ViewLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ViewLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
